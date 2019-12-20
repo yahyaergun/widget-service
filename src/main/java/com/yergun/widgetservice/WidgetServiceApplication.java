e306d82ef2258ae6f3fd316652e12027de543f17
Yahya Ergun <yahya.ergun@Yahyas-MacBook-Pro.local>
2019-12-20 16:22:10 +0100

    initial

---
+++ src/main/java/com/yergun/widgetservice/WidgetServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yergun.widgetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WidgetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WidgetServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yergun/widgetservice/WidgetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yergun.widgetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WidgetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
